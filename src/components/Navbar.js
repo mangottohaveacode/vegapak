aa444ec504a6f7e6d03e97a56afedbfc6c73dd95
Muamer Bandic <mbandic@protonmail.com>
2022-07-01 13:51:22 +0200

    changed everything, new nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,49 +2,36 @@ import React, { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  const navLinks = document.querySelectorAll(".nav-links li");
-
   const [isActive, setActive] = useState(false);
 
   const toggleClass = () => {
     setActive(!isActive);
-
-    navLinks.forEach((link, index) => {
-      console.log(index);
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 1.5
-        }s`;
-      }
-    });
   };
 
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <h4>THE NAV</h4>
-        </div>
-        <ul className={`nav-links ${isActive ? "nav-active" : ""}`}>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Work</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-        </ul>
-        <div className="burger" onClick={toggleClass}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
+      <nav className="navbar">
+        <div className="brand-title">Brand Name</div>
+        <a href="#" className="toggle-button" onClick={toggleClass}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
+        <div className={`navbar-links ${isActive ? "active" : ""}`}>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Work</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
